7e806369e03c072a5790a12dc45bc16af8e93beb
Den <karatygad@gmail.com>
2024-11-08 20:57:59 +0100

    lesson-2

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,31 +1,31 @@
-const age = 15;
-const name = 'Denys |';
-const isStudent = true;
-const myString =
-    "Любопытство никого не губило. Скорее наоборот, это одна из лучших вещей в жизни. — Джеймс Боуэн";
-let myNumber = 10;
-myNumber += 10;
+// const age = 15;
+// const name = 'Denys |';
+// const isStudent = true;
+// const myString =
+//     "Любопытство никого не губило. Скорее наоборот, это одна из лучших вещей в жизни. — Джеймс Боуэн";
+// let myNumber = 10;
+// myNumber += 10;
 
-const myNull = null;
-console.log(age, isStudent, myNumber, myNull);
-console.log(name, myString);
+// const myNull = null;
+// console.log(age, isStudent, myNumber, myNull);
+// console.log(name, myString);
 
-const userName = prompt("Введіть ваше ім'я:");
-if (userName) {
-  alert(`Привіт, ${userName}!`);
-}
+// const userName = prompt("Введіть ваше ім'я:");
+// if (userName) {
+//   alert(`Привіт, ${userName}!`);
+// }
 
-let confirmData = confirm("Ты человек?");
-if (confirmData == true) {
-  alert("Дякую за підтвердження!");
-} else if (confirmData == false) {
-  alert("Дію відмінено!");
-}
+// let confirmData = confirm("Ты человек?");
+// if (confirmData === true) {
+//   alert("Дякую за підтвердження!");
+// } else if (confirmData === false) {
+//   alert("Дію відмінено!");
+// }
 
-alert("Warning, need to confirm your actions");
-let isTrue = confirm("Are you confirm your actions?");
-if (isTrue == true) {
-  alert("Дякую за підтвердження!");
-} else if (isTrue == false) {
-  alert("Дію відмінено!");
-}
+// alert("Warning, need to confirm your actions");
+// let isTrue = confirm("Are you confirm your actions?");
+// if (isTrue === true) {
+//   alert("Дякую за підтвердження!");
+// } else if (isTrue === false) {
+//   alert("Дію відмінено!");
+// }
---
+++ js/lesson-2.js
@@ -0,0 +1,27 @@
+let mother = 30;
+console.log("Години: " + mother * 24, "Хвилини: " + 30 * 24 * 60);
+
+let health = 100 - 10;
+let energy = 100 - 12;
+console.log("Здоровье: " + health, "Энергия: " + energy)
+
+const price = 2312;
+const percent = 10;
+
+const newPrice = price - (price * percent) / 100;
+console.log(newPrice); // Формула из интеренета
+
+const float = 108.2183;
+console.log(Math.floor(float))
+
+let parse = '50.1212angularjavajaws';
+console.log(Number.parseFloat(parse))
+
+let cow = '50.1212angularjavajaws';
+console.log(Number.parseInt(cow))
+
+let fish = 4;
+console.log(Math.sqrt(fish))
+
+let cool = "221";
+console.log(toString() + parseInt(cool))
