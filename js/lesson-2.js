9145d06397e313761cdbd600c5d6acb7973e15b9
Den <karatygad@gmail.com>
2024-11-09 12:21:04 +0100

    farengeit

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -1,9 +1,12 @@
+const farengeit = 22 * 1.8 + 32;
+console.log(farengeit);
+
 let mother = 30;
 console.log("Години: " + mother * 24, "Хвилини: " + 30 * 24 * 60);
 
 let health = 100 - 10;
 let energy = 100 - 12;
-console.log("Здоровье: " + health, "Энергия: " + energy)
+console.log("Здоровье: " + health, "Энергия: " + energy);
 
 const price = 2312;
 const percent = 10;
@@ -12,16 +15,16 @@ const newPrice = price - (price * percent) / 100;
 console.log(newPrice); // Формула из интеренета
 
 const float = 108.2183;
-console.log(Math.floor(float))
+console.log(Math.floor(float));
 
-let parse = '50.1212angularjavajaws';
-console.log(Number.parseFloat(parse))
+let parse = "50.1212angularjavajaws";
+console.log(Number.parseFloat(parse));
 
-let cow = '50.1212angularjavajaws';
-console.log(Number.parseInt(cow))
+let cow = "50.1212angularjavajaws";
+console.log(Number.parseInt(cow));
 
 let fish = 4;
-console.log(Math.sqrt(fish))
+console.log(Math.sqrt(fish));
 
 let cool = "221";
-console.log(toString() + parseInt(cool))
+console.log(toString() + parseInt(cool));
