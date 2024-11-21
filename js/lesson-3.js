ff3de6392c481a28c17d5aaf2e8c27a891eceac0
Den <karatygad@gmail.com>
2024-11-21 22:46:06 +0100

    lesson-3

---
+++ js/lesson-3.js
@@ -0,0 +1,19 @@
+// 1
+const result = 5 + 5 + '5';
+console.log(typeof (result));
+
+const string_1 = "My ";
+const string_2 = "name ";
+const string_3 = "is ";
+
+const fullName = string_1 + string_2 + string_3 + "Viktor";
+console.log(fullName);
+// 2
+const email = "karatygad@gmail.com"
+console.log(email.indexOf("@"));
+console.log("Кількість символів: " + email.length);
+// 3
+const userName = "Den4k"
+const payment = 300;
+// 4
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
