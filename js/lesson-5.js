cacf23dd16de552d87774be8e476a612831e8761
Den <karatygad@gmail.com>
2024-11-29 23:13:31 +0100

    add lesson-5

---
+++ js/lesson-5.js
@@ -0,0 +1,137 @@
+// 1
+const cofe = "Кава",tea = "Чай", juice = "Сік";
+let drink = prompt(`Що ви будете пити? ${cofe}, ${tea}, ${juice}`)
+
+switch (drink) {
+    case "Кава":
+        alert("Ви обрали каву")
+        break;
+    case "Чай":
+        alert("Ви обрали чай")
+        break;
+    case "Сік":
+        alert("Ви обрали сік")
+        break;
+
+    default:
+        alert("Такого напою немае наразі")
+        break;
+}
+
+// 2
+let days = prompt("Напишіть день тижня");
+  
+switch (days) {
+  case "Субота":
+    alert("Вихідний");
+    break;
+  case "Неділя":
+    alert("Вихідний");
+    break;
+
+    default:
+      alert("Робочий день");
+      break;
+}
+
+// 3
+let month = parseInt(prompt("Введіть місяць"));
+
+switch (month) {
+  case 12:
+  case 1:
+  case 2:
+    alert("Зима");
+    break;
+  case 3:
+  case 4:
+  case 5:
+    alert("Весна");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    alert("Лето");
+    break;
+  case 9:
+  case 1:
+  case 11:
+    alert("Осень");
+    break;
+
+  default:
+    alert("Такого місяця нема");
+}
+
+// 4
+let emountDays = parseInt(prompt("Введіть місяць"));
+
+switch (emountDays) {
+  case 1:
+  case 3:
+  case 5:
+  case 7:
+  case 8:
+  case 10:
+  case 12:
+    alert("В цьому місяці 31 дней");
+    break;
+  case 4:
+  case 6:
+  case 9:
+  case 11:
+    alert("В цьому місяці 30 дней");
+    break;
+  case 2:
+    alert("В цьому місяці у нас тільки 28 днів");
+    break;
+  default:
+    alert("Такого місяця немае");
+    break;
+}
+
+// 5
+let color = prompt("Введіть колір").toLowerCase()
+
+switch (color) {
+    case "червоний":
+        alert("Stop")
+        break;
+    case "жовтий":
+        alert("Wait")
+        break;
+    case "зелений":
+        alert("Running");
+        break;
+    
+    default:
+        alert("Я не придумал действие для этого цвета")
+        break;
+}
+
+// 6
+const number = parseFloat(prompt("Первое число (Можливо ділити на нуль)"));
+const number2 = parseFloat(prompt("Второе число"));
+const operator = prompt("Ведите оператора (+, -, /, *)");
+let result;
+
+switch (operator) {
+  case "+":
+    result = number + number2;
+    alert(result);
+    break;
+  case "-":
+    result = number - number2;
+    alert(result);
+    break;
+  case "*":
+    result = number * number2;
+    alert(result);
+    break;
+  case "/":
+    result = number / number2;
+    alert(result);
+    break;
+  default:
+    alert("Неправильно написан оператор");
+}
