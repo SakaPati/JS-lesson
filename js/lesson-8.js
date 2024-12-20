2e40617c61966d622718b74952c5bb9430101f88
Denis Karatyha <karatygad@gmail.com>
2024-12-20 21:39:40 +0100

    start lesson-8

---
+++ js/lesson-8.js
@@ -0,0 +1,24 @@
+// 1
+const friends = ["Alex", "Denis", "Maksim"];
+
+let string = '';
+
+for (let i = 0; i < friends.length; i++) {
+    const a = friends.join(", ")
+    console.log(a);
+};``
+
+// 2
+const cards = [
+    "Карточка-1",
+    "Карточка-2",
+    "Карточка-3",
+    "Карточка-4",
+    "Карточка-5"
+];
+
+const cardToRemove = cards.splice(2, 1)
+const cardToInsert = cards.splice(5, 0, "Карточка-6")
+const cardToUpdata = cards.splice(3, 0, "Updata")
+
+console.log(cards);
